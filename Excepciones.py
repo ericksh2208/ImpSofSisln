8c9a9de60067952badbb45d296d2b64e94cb7189
ericksh2208 <soto.erick.2024@cbtis-246.edu.mx>
2025-02-28 19:50:17 -0600

    Add files via upload

---
+++ Excepciones.py
@@ -0,0 +1,81 @@
+#Erick Otoniel Soto Hdez
+#Excepciones
+#28/02/25
+#2do "J"
+
+
+
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("hola"
+
+
+# In[3]:
+
+
+x = 10 / 0
+
+
+# In[5]:
+
+
+print(x)
+
+
+# In[9]:
+
+
+lista = [1, 2, 3]
+print(lista[4])
+
+
+# In[11]:
+
+
+x = "Hola" + 5
+
+
+# In[17]:
+
+
+edad = 20
+if edad > 18:
+print("La edad es superior a 18")
+   print("Puede conducir")
+si no
+    print("La edad es inferior a 18")
+
+
+# In[19]:
+
+
+num = 1445
+cap = num.capitalize()
+print(cap)
+
+
+# In[33]:
+
+
+ciudades = {"Canadá": "Ottawa", "EE.UU.": "Washington", "Italia": "Roma"}
+print(ciudades["mexico"])
+
+
+# In[35]:
+
+
+file1 = open ("nontes","r")
+
+
+# In[67]:
+
+
+def factorial(n):
+    return n * factorial(n-1)
+
+print(factorial(5))
+
